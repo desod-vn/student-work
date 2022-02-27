33beca6e2c60f0ba4ec6d6cd78c80d7de1558e78
Desod <thangdotpro@gmail.com>
2022-02-27 15:31:10 +0700

    update: crud category

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace StudentWork.Models
 {
     [Table("Category")]
@@ -9,14 +10,17 @@ namespace StudentWork.Models
         public int Id {set; get;}
     
         [StringLength(255)]
+        [Display(Name = "Hình ảnh")]
         public string Image {set; get;}
         
         [Required]
         [StringLength(255)]
+        [Display(Name = "Tên chuyên mục")]
         public string Name {set; get;}
 
         [Required]
         [StringLength(255)]
+        [Display(Name = "Mô tả")]
         public string Description {set; get;}
     }
 }

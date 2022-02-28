9ec6b78803d295f2173948336b42d355ded91b73
Desod <thangdotpro@gmail.com>
2022-02-28 13:33:29 +0700

    update: draw home ui

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using StudentWork.Models;
 
@@ -12,14 +13,19 @@ namespace StudentWork.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly StudentWorkContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, StudentWorkContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewBag.Funds = await _context.funds.ToListAsync();
+            ViewBag.d = 's';
+
             return View();
         }
 
